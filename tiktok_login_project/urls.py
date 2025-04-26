0a9cc287aa6764f422844803d6937c040c5e92ee
youngdd4 <youngdd4@gmail.com>
2025-04-26 21:44:35 +0300

    Fix: Added home page route for domain root3

--- tiktok_login_project/urls.py
+++ tiktok_login_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import redirect
 
 
 urlpatterns = [
   
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # Include accounts URLs at root
+    path('', lambda request: redirect('accounts:login'), name='home'),  # Add this line for root URL redirect
+
 ]
