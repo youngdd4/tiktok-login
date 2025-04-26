72a6a97599dceb7b83ce298a070b5995288c2bf9
youngdd4 <youngdd4@gmail.com>
2025-04-26 19:12:33 +0300

    Initial commit for TikTok Login project

--- tiktok_login_project/settings.py
+++ tiktok_login_project/settings.py
@@ -40,7 +40,23 @@ MIDDLEWARE = [
 ]
 
 # ... (rest of your settings)
+# In settings.py
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Make sure this path exists
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # Database settings - use dj_database_url to handle DATABASE_URL
 DATABASES = {
     'default': dj_database_url.config(
