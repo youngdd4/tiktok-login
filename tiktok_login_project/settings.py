7b7072b3f28aa2f002f121de548308ebc7f3d1d3
youngdd4 <youngdd4@gmail.com>
2025-04-26 19:48:54 +0300

    Fix: Added home page route for domain root

--- tiktok_login_project/settings.py
+++ tiktok_login_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,tiktok-login-9q4p.onrender.com,www.emmanueltech.store,emmanueltech.store').split(',')
 
 # Application definition
 INSTALLED_APPS = [
--- tiktok_login_project/urls.py
+++ tiktok_login_project/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to Emmanuel Tech!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
