92975068b09799eb53106410f50510a0299e2bea
youngdd4 <youngdd4@gmail.com>
2025-04-26 21:32:53 +0300

    Fix: Added home page route for domain root

--- tiktok_login_project/urls.py
+++ tiktok_login_project/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to Emmanuel Tech!")
+
+
 urlpatterns = [
     path('', home, name='home'),  # root URL ('/') points to a simple view
     path('admin/', admin.site.urls),
